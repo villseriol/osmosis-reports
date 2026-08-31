8f85cf80a98c3fdd0aea6293db344f5df7a2a771
EY4180 <44942476+EY4180@users.noreply.github.com>
2026-09-01 07:42:30 +0800

    Scaffolding Future Implementation

---
+++ src/main/java/io/github/villseriol/osmosis/reports/v0_6/config/ReportAlias.java
@@ -0,0 +1,41 @@
+// This software is released into the Public Domain.  See copying.txt for details.
+package io.github.villseriol.osmosis.reports.v0_6.config;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+
+public enum ReportAlias {
+    /**
+     * TODO.
+     */
+    CHARACTER_FREQUENCY("character-frequency"),
+    /**
+     * TODO.
+     */
+    CHARACTER_BLOCK_FREQUENCY("character-block-frequency");
+
+    private final String alias;
+
+    ReportAlias(String alias) {
+        this.alias = alias;
+    }
+
+
+    @JsonValue
+    public String getAlias() {
+        return alias;
+    }
+
+
+    @JsonCreator
+    public static ReportAlias fromAlias(String alias) {
+        for (ReportAlias value : values()) {
+            if (value.alias.equals(alias)) {
+                return value;
+            }
+        }
+
+        throw new IllegalArgumentException("Unknown normalize alias: " + alias);
+    }
+}
