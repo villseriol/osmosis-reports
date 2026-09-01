ad305cf8652d518dd46a936feab4488d8acda612
EY4180 <44942476+EY4180@users.noreply.github.com>
2026-09-01 18:55:46 +0800

    Added Unit Test For Configuration Loading

---
+++ src/test/java/io/github/villseriol/v0_6/loader/XmlLoaderTest.java
@@ -0,0 +1,67 @@
+// This software is released into the Public Domain.  See copying.txt for details.
+package io.github.villseriol.v0_6.loader;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+
+import org.junit.Test;
+import org.openstreetmap.osmosis.testutil.AbstractDataTest;
+
+import io.github.villseriol.osmosis.reports.v0_6.config.OsmosisReportsConfig;
+import io.github.villseriol.osmosis.reports.v0_6.config.ReportAlias;
+import io.github.villseriol.osmosis.reports.v0_6.config.ReportNode;
+import io.github.villseriol.osmosis.reports.v0_6.loader.XmlLoader;
+
+
+public class XmlLoaderTest extends AbstractDataTest {
+    private final XmlLoader<OsmosisReportsConfig> loader = XmlLoader.getInstance(OsmosisReportsConfig.class);
+
+    @Test
+    public void testLoadEmptyConfiguration() {
+        File configFile = dataUtils.createDataFile("v0_6/empty-configuration.xml");
+
+        OsmosisReportsConfig config = loader.load(configFile);
+
+        assertNotNull(config);
+        assertNotNull(config.getReports());
+        assertTrue(config.getReports().isEmpty());
+    }
+
+
+    @Test
+    public void testLoadSingleReportConfiguration() {
+        File configFile = dataUtils.createDataFile("v0_6/single-report-configuration.xml");
+
+        OsmosisReportsConfig config = loader.load(configFile);
+
+        assertNotNull(config);
+        assertEquals(1, config.getReports().size());
+
+        ReportNode report = config.getReports().get(0);
+        assertEquals(ReportAlias.CHARACTER_FREQUENCY, report.getAlias());
+        assertEquals("character-frequency.xlsx", report.getOutputPath());
+        assertEquals(1, report.getTags().size());
+        assertEquals("name", report.getTags().get(0).getKey());
+    }
+
+
+    @Test
+    public void testLoadIncompleteReportConfiguration() {
+        File configFile = dataUtils.createDataFile("v0_6/incomplete-report-configuration.xml");
+
+        OsmosisReportsConfig config = loader.load(configFile);
+
+        assertNotNull(config);
+        assertEquals(1, config.getReports().size());
+
+        ReportNode report = config.getReports().get(0);
+        assertNull(report.getAlias());
+        assertNull(report.getOutputPath());
+        assertNotNull(report.getTags());
+        assertTrue(report.getTags().isEmpty());
+    }
+}
